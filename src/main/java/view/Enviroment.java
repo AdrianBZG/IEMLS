da871b1cf51e0fa3b76c72c64e341265cbcd2749
Eleazar <eleazardzdo@gmail.com>
2016-11-04 19:34:37 +0000

    add moeaframework, rxjavafx

---
+++ src/main/java/view/Enviroment.java
@@ -0,0 +1,14 @@
+/**
+ * IEMLS - Enviroment.java 4/11/16
+ * <p>
+ * Copyright 20XX Eleazar Díaz Delgado. All rights reserved.
+ */
+
+package view;
+
+/**
+ * TODO: Commenta algo
+ *
+ */
+public class Enviroment {
+}
