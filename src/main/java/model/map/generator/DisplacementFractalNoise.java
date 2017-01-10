45f0d991148944f16f46de4052330ec2ef4e24e7
AdrianBZG <arodriba@ull.es>
2017-01-10 17:09:07 +0000

    Fixing Displacement Fractal Noise

--- src/main/java/model/map/generator/DisplacementFractalNoise.java
+++ src/main/java/model/map/generator/DisplacementFractalNoise.java
@@ -2,107 +2,106 @@ package model.map.generator;
 
 import java.util.Random;
 
-/**
- * Taken from a Stefan Gustavson gist
- *
- * TODO: Improve
- * TODO: Touch gradients to get special walls
- */
 public class DisplacementFractalNoise implements IGenerator {
-    public int[][] grad3 = {{1,1,0},{-1,1,0}, {1,-1,0}, {-1,-1,0},
-                     {1,0,1}, {-1,0,1}, {1,0,-1}, {-1,0,-1},
-                     {0,1,1}, {0,-1,1}, {0,1,-1}, {0,-1,-1}};
+    /** Source of entropy */
+    private Random rand_;
 
-    private final double F2 = 0.5*(Math.sqrt(3.0)-1.0);
-    private final double G2 = (3.0-Math.sqrt(3.0))/6.0;
+    /** Amount of roughness */
+    float roughness_;
 
-    private short perm[] = new short[512];
-    private short permMod12[] = new short[512];
+    /** Plasma fractal grid */
+    private float[][] grid_;
+
+    private float[][] normalizedGrid_;
+
+    private long seed;
+
+    private boolean generated_ = false;
 
     public DisplacementFractalNoise() {
-        calculateRamdonValues();
+    }
+
+    private void initialize() {
+        //roughness_ = (float)0.5 / EnvironmentMap.getDimensions().get().getWidth();
+        roughness_ = 0.8f;
+        //grid_ = new float[EnvironmentMap.getDimensions().get().getWidth()][EnvironmentMap.getDimensions().get().getHeight()];
+        grid_ = new float[1000][1000];
+        rand_ = new Random();
+
+        int xh = grid_.length - 1;
+        int yh = grid_[0].length - 1;
+
+        // set the corner points
+        grid_[0][0] = rand_.nextFloat() - 0.05f;
+        grid_[0][yh] = rand_.nextFloat() - 0.05f;
+        grid_[xh][0] = rand_.nextFloat() - 0.05f;
+        grid_[xh][yh] = rand_.nextFloat() - 0.05f;
+
+        // generate the fractal
+        generate(0, 0, xh, yh);
+
+        normalizedGrid_ = this.toNormalized();
+
+        generated_ = true;
+    }
+
+    // Add a suitable amount of random displacement to a point
+    private float roughen(float v, int l, int h) {
+        return v + roughness_ * (float) (rand_.nextGaussian() * (h - l));
+    }
+
+
+    // generate the fractal
+    private void generate(int xl, int yl, int xh, int yh) {
+        int xm = (xl + xh) / 2;
+        int ym = (yl + yh) / 2;
+        if ((xl == xm) && (yl == ym)) return;
+
+        grid_[xm][yl] = 0.5f * (grid_[xl][yl] + grid_[xh][yl]);
+        grid_[xm][yh] = 0.5f * (grid_[xl][yh] + grid_[xh][yh]);
+        grid_[xl][ym] = 0.5f * (grid_[xl][yl] + grid_[xl][yh]);
+        grid_[xh][ym] = 0.5f * (grid_[xh][yl] + grid_[xh][yh]);
+
+        float v = roughen(0.5f * (grid_[xm][yl] + grid_[xm][yh]), xl + yl, yh
+                + xh);
+        grid_[xm][ym] = v;
+        grid_[xm][yl] = roughen(grid_[xm][yl], xl, xh);
+        grid_[xm][yh] = roughen(grid_[xm][yh], xl, xh);
+        grid_[xl][ym] = roughen(grid_[xl][ym], yl, yh);
+        grid_[xh][ym] = roughen(grid_[xh][ym], yl, yh);
+
+        generate(xl, yl, xm, ym);
+        generate(xm, yl, xh, ym);
+        generate(xl, ym, xm, yh);
+        generate(xm, ym, xh, yh);
+    }
+
+    public float[][] toNormalized() {
+        int w = grid_.length;
+        int h = grid_[0].length;
+        float[][] ret = new float[w][h];
+        for(int i = 0;i < w;i++) {
+            for(int j = 0;j < h;j++) {
+                if(grid_[i][j] > 0 && grid_[i][j] < 2) {
+                    ret[i][j] = (float)0.6;
+                } else if ((grid_[i][j] > 2 && grid_[i][j] < 6) || grid_[i][j] < 0) {
+                    ret[i][j] = (float)0.1;
+                }
+            }
+        }
+        return ret;
     }
 
     @Override
     public void newSeed() {
-        calculateRamdonValues();
-    }
-
-    private void calculateRamdonValues() {
-        Random rand = new Random();
-        for(int i=0; i<512; i++)
-        {
-            perm[i]= (short)rand.nextInt(255);
-            permMod12[i] = (short)(perm[i] % 12);
-        }
-    }
-
-
-    // This method is a *lot* faster than using (int)Math.floor(x)
-    private int fastfloor(double x) {
-        int xi = (int)x;
-        return x<xi ? xi-1 : xi;
-    }
-
-    private double dot(int[] g, double x, double y) {
-        return g[0]*x + g[1]*y; }
-
-    public double noise(double xin, double yin) {
-        double n0, n1, n2; // Noise contributions from the three corners
-        // Skew the input space to determine which simplex cell we're in
-        double s = (xin+yin)*F2; // Hairy factor for 2D
-        int i = fastfloor(xin+s);
-        int j = fastfloor(yin+s);
-        double t = (i+j)*G2;
-        double X0 = i-t; // Unskew the cell origin back to (x,y) space
-        double Y0 = j-t;
-        double x0 = xin-X0; // The x,y distances from the cell origin
-        double y0 = yin-Y0;
-        // For the 2D case, the simplex shape is an equilateral triangle.
-        // Determine which simplex we are in.
-        int i1, j1; // Offsets for second (middle) corner of simplex in (i,j) coords
-        if(x0>y0) {i1=1; j1=0;} // lower triangle, XY order: (0,0)->(1,0)->(1,1)
-        else {i1=0; j1=1;}      // upper triangle, YX order: (0,0)->(0,1)->(1,1)
-        // A step of (1,0) in (i,j) means a step of (1-c,-c) in (x,y), and
-        // a step of (0,1) in (i,j) means a step of (-c,1-c) in (x,y), where
-        // c = (3-sqrt(3))/6
-        double x1 = x0 - i1 + G2; // Offsets for middle corner in (x,y) unskewed coords
-        double y1 = y0 - j1 + G2;
-        double x2 = x0 - 1.0 + 2.0 * G2; // Offsets for last corner in (x,y) unskewed coords
-        double y2 = y0 - 1.0 + 2.0 * G2;
-        // Work out the hashed gradient indices of the three simplex corners
-        int ii = i & 255;
-        int jj = j & 255;
-        int gi0 = permMod12[ii+perm[jj]];
-        int gi1 = permMod12[ii+i1+perm[jj+j1]];
-        int gi2 = permMod12[ii+1+perm[jj+1]];
-        // Calculate the contribution from the three corners
-        double t0 = 0.5 - x0*x0-y0*y0;
-        if(t0<0) n0 = 0.0;
-        else {
-            t0 *= t0;
-            n0 = t0 * t0 * dot(grad3[gi0], x0, y0);  // (x,y) of grad3 used for 2D gradient
-        }
-        double t1 = 0.5 - x1*x1-y1*y1;
-        if(t1<0) n1 = 0.0;
-        else {
-            t1 *= t1;
-            n1 = t1 * t1 * dot(grad3[gi1], x1, y1);
-        }
-        double t2 = 0.5 - x2*x2-y2*y2;
-        if(t2<0) n2 = 0.0;
-        else {
-            t2 *= t2;
-            n2 = t2 * t2 * dot(grad3[gi2], x2, y2);
-        }
-        // Add contributions from each corner to get the final noise value.
-        // The result is scaled to return values in the interval [-1,1].
-        return 70.0 * (n0 + n1 + n2);
+        seed = new Random().nextInt();
     }
 
     @Override
     public double generateAtPoint(double x, double y) {
-        return noise(x, y);
+        if(!generated_) initialize();
+        //System.out.println("Retorno:" + grid_[(int)x][(int)y]);
+        return normalizedGrid_[(int)x][(int)y];
     }
 
     @Override
